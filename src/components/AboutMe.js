aa0c2f5e24d5d2befcc89b44ffa7dd3dfec7b36a
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-07-04 14:27:35 -0500

    about me section complete

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import profileImage from '../assets/sticker.png';
 
 const AboutMe = () => {
   return (
     <div className="card-container">
-      <div className="card-content">
-        <img
-          src="https://example.com/profile-image.png"
-          alt="Profile"
-          className="w-32 h-32 rounded-full mx-auto mb-4"
-        />
+      <div className="card-content flex flex-col items-center">
+        <div className="flex items-center justify-center">
+          <img
+            src={profileImage}
+            alt="Profile"
+            className="w-24 h-auto max-w-30% rounded-30% mb-4"
+            style={{ maxWidth: '100%' }}
+          />
+        </div>
         <h2 className="text-xl font-bold mb-2">About Me</h2>
         <p className="text-gray-700">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          hendrerit eu risus et tristique. Nullam posuere consectetur ligula,
-          eget blandit purus finibus sed. Quisque quis sollicitudin tortor.
-          Donec dictum sapien a mi feugiat, et consectetur mauris facilisis.
+          Hi, my name is Adrian. 
+          I am a recent transplant to the world of programming.
+          With a backround in home remodeling, it's safe to say I am treading into a new world. I am excitred to begin my journery into coding and hope
+          to expand my knowledge during and after my coding bootcamp. 
         </p>
       </div>
     </div>
